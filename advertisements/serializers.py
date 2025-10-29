e47359f94ddc7c63042702aaacf254f385c8567f
mohammed99-m <markoriobrazil@gmail.com>
2025-10-29 23:15:10 +0300

    fix

--- advertisements/serializers.py
+++ advertisements/serializers.py
@@ -1,31 +1,16 @@
 # serializers.py
 from rest_framework import serializers
 from .models import Advertisement
-from cloudinary.utils import cloudinary_url
 
 class AdvertisementSerializer(serializers.ModelSerializer):
-    image_public_id = serializers.CharField(write_only=True, required=False, allow_null=True)
-    image_url_full = serializers.SerializerMethodField(read_only=True)
+    image_url_full = serializers.SerializerMethodField()
 
     class Meta:
         model = Advertisement
-        fields = ['id','title','content','image_url','image_url_full','date','is_active','image_public_id']
-        read_only_fields = ['id','date']
+        fields = ['id', 'title', 'content', 'image_url', 'image_url_full', 'date', 'is_active']
 
     def get_image_url_full(self, obj):
-        try:
-            public_id = obj.image_url
-            if not public_id:
-                return None
-            url, _ = cloudinary_url(public_id, secure=True)
-            return url
-        except Exception:
-            return None
-
-    def create(self, validated_data):
-        public_id = validated_data.pop('image_public_id', None)
-        instance = Advertisement.objects.create(**validated_data)
-        if public_id:
-            instance.image_url = public_id
-            instance.save(update_fields=['image_url'])
-        return instance
+        if obj.image_url:
+            # CloudinaryField provides .url for the full URL
+            return obj.image_url.url
+        return None
